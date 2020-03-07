51905010cf51d22d790837dfcbc26c0781088108
Jhonatan <jhonatancardoso09@gmail.com>
2020-03-07 20:53:29 -0300

    Updates

---
+++ precache-manifest.636e5e61848c5b283538881a759b71ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682dc786398fd53d9b25ad88aa5f402c",
+    "url": "/TestCoreBiz/index.html"
+  },
+  {
+    "revision": "290e1b6d30833130b8fe",
+    "url": "/TestCoreBiz/static/css/main.b617ba21.chunk.css"
+  },
+  {
+    "revision": "684800d47a8419fab993",
+    "url": "/TestCoreBiz/static/js/2.63227972.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/TestCoreBiz/static/js/2.63227972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290e1b6d30833130b8fe",
+    "url": "/TestCoreBiz/static/js/main.e12e3e91.chunk.js"
+  },
+  {
+    "revision": "7385d93b89c5c1a5fe90",
+    "url": "/TestCoreBiz/static/js/runtime-main.8c0ba4d1.js"
+  }
+]);
